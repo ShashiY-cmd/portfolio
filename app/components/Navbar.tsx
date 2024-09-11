2a167b26da0578a1ed1f245f6dfc157975af986e
sa <shashiyadavcm@gmail.com>
2024-09-11 23:00:30 +0530

    Nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,12 @@
 export function Navbar() {
   return (
-    <nav className=" max-w-7xl mx-auto px-4 md:px-8">
-      <h1>hello</h1>
+    <nav className=" max-w-7xl mx-auto px-4 md:px-8 py-5 grid grid-cols-12">
+      <div className=" col-span-6 flex md:col-span-3">
+        <link rel="stylesheet" href="/" />
+        <h1 className="text-3xl font-semibold">
+          Shashi <span className="text-blue-500">Yadav</span>
+        </h1>
+      </div>
     </nav>
   );
 }
